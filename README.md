da55db32a14e8b2fbf4c4e34a1752b472d4357b4
Yarmo Mackenbach <yarmo@protonmail.com>
2019-09-05 15:55:23 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # includeZero
-Include the [0 0] coordinates in a plot
+
+Include the [0 0] coordinates in a plot.
+
+## Usage
+
+`includeZero()` makes sure the plot will always show [0 0] by plotting an invisible point.  
+`h = includeZero()` returns the handle to the invisible point.  
+`includeZero(x, y)` overrules the coordinates for the invisible point.
---
+++ includeZero.m
@@ -0,0 +1,24 @@
+function h = includeZero(varargin)
+%INCLUDEZERO Include the [0 0] coordinates in a plot
+%   includeZero() makes sure the plot will always show [0 0] by plotting an
+%   invisible point.
+%
+%   h = includeZero() returns the handle to the invisible point.
+%
+%   includeZero(x, y) overrules the coordinates for the invisible point.
+
+    % Handle input/output
+    p = inputParser();
+    p.addOptional('x', 0);
+    p.addOptional('y', 0);
+    p.parse(varargin{:});
+    p = p.Results;
+    
+    % Plot the point and make it invisible
+    h = plot(p.x, p.y, '.w');
+    set(h, 'LineStyle', 'none', 'Marker', 'none');
+    
+    % Prevent a legend entry
+    set(get(get(h, 'Annotation'), 'LegendInformation'), 'IconDisplayStyle', 'off');
+
+end
